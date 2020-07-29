fa6eb62e14250b1fa367043b6b7fc79429ac15f8
ShiGuangzhao <guangzhao_shi@163.com>
2020-07-29 19:57:59 +0800

    寻宝刚开始

---
+++ 0每日一题/13_LCP_寻宝.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+    > File Name: 13_LCP_寻宝.cpp
+    > Author: ShiGuangzhao
+    > Mail: Guangzhao_Shi@163.com 
+    > Created Time: 2020年07月29日 星期三 10时14分55秒
+ ************************************************************************/
+
+#include <iostream>
+#include <vector>
+
+using std::vector;
+using std::string;
+
+class Solution {
+public:
+    int minimalSteps(vector<string>& maze) {
+
+    }
+};
