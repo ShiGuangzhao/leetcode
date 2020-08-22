e4a1cb8f59cec45091744f4c4205f3c41c78751c
ShiGuangzhao <Guangzhao_Shi@163.com>
2020-08-22 11:38:08 +0800

    21 合并两个链表

---
+++ 0每日一题/21_合并两个有序列表_循环.cpp
@@ -0,0 +1,59 @@
+/*************************************************************************
+    > File Name: 21_合并两个有序列表.cpp
+    > Author: ShiGuangzhao
+    > Mail: Guangzhao_Shi@163.com 
+    > Created Time: 2020年08月22日 星期六 10时26分32秒
+ ************************************************************************/
+
+#include <iostream>
+using namespace std;
+
+
+// Definition for singly-linked list.
+struct ListNode {
+    int val;
+    ListNode *next;
+    ListNode() : val(0), next(nullptr) {}
+    ListNode(int x) : val(x), next(nullptr) {}
+    ListNode(int x, ListNode *next) : val(x), next(next) {}
+};
+
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *ret;
+        // 空指针的情况
+        if(l1 == nullptr) {
+            return l2;
+        }
+        else if(l2 == nullptr) {
+            return l1;
+        }
+        // 都不是空指针，找的小的一个l，ret等于小的l
+        // l = l->next，直到l->next > 另一个la
+        // 如果上一步l->next等于nullptr，l->next = la，返回
+        // 如果找到大于la的，则l->next = mergeLists(la, l->next)
+        else {
+            ListNode *lsmall, *lgreat;
+            ret = lsmall = (l1->val <= l2->val)? l1: l2;
+            lgreat = (l1->val <= l2->val)? l2: l1;
+            while(true) {
+                if(lsmall->next == nullptr) {
+                    lsmall->next = lgreat;
+                    break;
+                }
+                if(lsmall->next->val <= lgreat->val) {  // 如果不包含=，遇到=时陷入死循环
+                    lsmall = lsmall->next;
+                }
+                else {
+                    // 交换lsmall->next   lgreat
+                    auto temp = lsmall->next;
+                    lsmall->next = lgreat;
+                    lgreat = temp;
+                    lsmall = lsmall->next;
+                }
+            }
+        }
+        return ret;
+    }
+};
