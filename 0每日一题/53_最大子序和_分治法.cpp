affa9c6a67b3116fbd60eede5f48cdbd35ff29f6
ShiGuangzhao <guangzhao_shi@163.com>
2020-08-27 20:07:16 +0800

    53分治法还在写

--- 0每日一题/53_最大子序和_分治法.cpp
+++ 0每日一题/53_最大子序和_分治法.cpp
@@ -11,11 +11,25 @@ using namespace std;
 
 // a[n], n = 0, 1, 2, n-1;
 // 分治法：0 -- n/2, 1 + n/2 -- n-1
-// 左边最大值，右边最大值，旁边测的最大值
+// 左边最大值，右边最大值，旁边两侧的最大值
+// 分：分成左右两部分，剩两个元素时停止
+// 治：
 
 class Solution {
 private:
-    void devide(vector<int> &nums, int &maxSum, int &sideMaxSum) {
+    // int sum, bool side
+    using SumType = pair<int, bool>;
+    void devide_conquer(
+            vector<int>::iterator &beg, vector<int>::iterator &end, int &maxSum, SumType &lsideMaxSum, SumType &rsideMaxSum) {
+        if(end - beg == 2) {
+            maxSum = max(*beg, *(beg + 1));
+
+        }
+        else if(end - beg == 1) {
+
+        }
+
+
 
     }
 public:
